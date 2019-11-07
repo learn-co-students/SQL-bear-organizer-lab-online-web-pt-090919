be9f57779c41128e81c096a1ee964e646eb7b4b9
Jared S <jaredjames2020@gmail.com>
2019-11-07 01:03:58 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,13 +19,14 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT (*) FROM bears "
+  SELECT breed, owner_id, COUNT(breed) FROM cats GROUP BY breed, owner_id;
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(*) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT * FROM bears WHERE id = 8"
 end
